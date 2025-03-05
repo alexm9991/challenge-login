fe6aa8a9897205391d467f6df6a5f026d77d8753
Alexandra Merchan <amerchan@lucasian.com>
2025-03-04 20:59:18 -0500

    ✨ Schemas: add object schema validation

--- src/lib/schemas/user-register-schema.js
+++ src/lib/schemas/user-register-schema.js
@@ -4,15 +4,19 @@ const schema = yup.object().shape({
   email: yup
     .string()
     .email("Correo inválido")
-    .required("El correo es obligatorio"),
-  password: yup
+    .required("El correo electrónico es obligatorio"),
+    password: yup
     .string()
-    .min(6, "La contraseña debe tener al menos 6 caracteres")
+    .min(8, "La contraseña debe tener al menos 8 caracteres")
+    .matches(/[A-Z]/, "Debe contener al menos una letra mayúscula")
+    .matches(/[a-z]/, "Debe contener al menos una letra minúscula")
+    .matches(/\d/, "Debe contener al menos un número")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/, "Debe contener al menos un carácter especial")
     .required("La contraseña es obligatoria"),
   confirmPassword: yup
     .string()
     .oneOf([yup.ref("password"), null], "Las contraseñas deben coincidir")
-    .required("La confirmacion de  la contraseña es obligatoria"),
+    .required("La confirmacion de la contraseña es obligatoria"),
 });
 
 const defaultValues = {
